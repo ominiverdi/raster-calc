cf43e765e38dee59437797b6747b2883585469c2
ominiverdi <lorenzo@ominiverdi.com>
2025-04-19 17:29:50 +0200

    removing fixed_points

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,3 +1,2 @@
 // src/utils/mod.rs
 pub mod gdal_ext;
-pub mod fixed_point;
--- tests/unit_tests.rs
+++ tests/unit_tests.rs
@@ -1,13 +1,1 @@
 // tests/unit_tests.rs
-use raster_calc::utils::fixed_point::to_fixed_point;
-
-#[test]
-fn test_fixed_point_conversion() {
-    let input = vec![0.0, 0.5, -0.5, 1.0, -1.0, -999.0];
-    let scale = 10000;
-    let nodata = -10000;
-    
-    let result = to_fixed_point(&input, scale, nodata);
-    
-    assert_eq!(result, vec![0, 5000, -5000, 9999, -9999, -10000]);
-}
